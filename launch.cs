12cb215db07f72d72648e0703f281374f0e833a9
prehaxv <npidros@gmail.com>
2026-05-28 15:13:27 +0400

    try-catch

--- launch.cs
+++ launch.cs
@@ -1,14 +1,22 @@
   public static Process Start(ProcessStartInfo startInfo)
   {
-    Process process = new Process();
-    if (startInfo == null)
+    try
     {
-      throw new ArgumentNullException("startInfo");
+      Process process = new Process();
+      if (startInfo == null)
+      {
+        throw new ArgumentNullException("startInfo");
+      }
+      process.StartInfo = startInfo;
+      if (process.Start())
+      {
+        return process;
+      }
     }
-    process.StartInfo = startInfo;
-    if (process.Start())
+    catch(Exception ex)
     {
-      return process;
+        Console.WriteLine($"failed to start, {ex.Message}");      
     }
+    
     return null;
   }
